8ef1c5e3b95d0c9eb92260d0f7d754e92b54cb95
Rishabh5002 <myselfrish@gmail.com>
2023-12-25 19:28:14 +0530

    To do list finished

--- Projects/TO DO App/todo.js
+++ Projects/TO DO App/todo.js
@@ -1,5 +1,29 @@
+let todoList = [];
+displayItems();
+
 function addTodo() {
   let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
-  console.log(todoItem);
+  let todoDate = dateElement.value;
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+
+  displayItems();
 } 
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHTML = '';
+  for (let i = 0; i < todoList.length; i++){
+    let {item,dueDate} = todoList[i];
+    newHTML += `
+    <span> ${item}</span>
+    <span> ${dueDate}</span>
+    <button class = "delete-Todo" onclick = "todoList.splice (${i}, 
+       1);
+       displayItems();" >Delete</button>`
+  }
+  containerElement.innerHTML = newHTML;
+}
